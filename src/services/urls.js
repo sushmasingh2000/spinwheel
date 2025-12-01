05c3f780eb539bb6e6da25ad67a7fd5518937c91
sushmasingh2000 <sushmasingh16166@gmail.com>
2025-12-01 16:26:26 +0530

    fjgd

--- src/services/urls.js
+++ src/services/urls.js
@@ -4,7 +4,7 @@
 
 export const frontened = "https://spinwheel-chi.vercel.app";
 export const meta = "spinwheel-chi.vercel.app";
-export const game_domain = "http://192.168.18.214:9005";
+export const game_domain = "https://one.raosonfilms.com";
 
 export const endpoint = {
   member_user_registration: `${game_domain}/api/v1/member-user-registration`,
